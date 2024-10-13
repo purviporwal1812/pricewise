02b963cb7ec274b01d27ecee6c6c9e8b0f2103a5
Purvi Porwal <purviporwal1812@gmail.com>
2024-10-14 01:00:31 +0530

    mongodb connected

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 // Middleware
 app.use(cors());
